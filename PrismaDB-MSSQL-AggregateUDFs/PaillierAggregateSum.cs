eefabbd9b9bf470b3702fa26f0d3b3450dce29f7
Calvin Che <calvin@cczy.io>
2018-10-27 22:31:45 +0800

    2 bigint accumulator

--- PrismaDB-MSSQL-AggregateUDFs/PaillierAggregateSum.cs
+++ PrismaDB-MSSQL-AggregateUDFs/PaillierAggregateSum.cs
@@ -15,7 +15,8 @@ namespace PrismaDB.MSSQL.AggregateUDFs
             MaxByteSize = 8000)]
     public class PaillierAggregateSum : IBinarySerialize
     {
-        private byte[] accumulator;
+        private BigInteger accumulatorActual;
+        private BigInteger accumulatorNeg;
         private BigInteger cachedNSq;
         private bool isEmpty;
 
@@ -26,17 +27,26 @@ namespace PrismaDB.MSSQL.AggregateUDFs
 
         public void Accumulate(SqlBytes p_toAdd, SqlBytes p_NSquare)
         {
-            var bi_toAdd = p_toAdd.Buffer;
+            var bytes = p_toAdd.Buffer;
+            var bytesActual = new byte[bytes.Length / 2];
+            Array.Copy(bytes, bytesActual, bytes.Length / 2);
+            var bytesNegative = new byte[bytes.Length / 2];
+            Array.Copy(bytes, bytes.Length / 2, bytesNegative, 0, bytes.Length / 2);
+
+            var biActual_toAdd = new BigInteger(bytesActual);
+            var biNegative_toAdd = new BigInteger(bytesNegative);
 
             if (isEmpty)
             {
-                accumulator = bi_toAdd;
+                accumulatorActual = biActual_toAdd;
+                accumulatorNeg = biNegative_toAdd;
                 cachedNSq = new BigInteger(p_NSquare.Buffer);
                 isEmpty = false;
                 return;
             }
 
-            accumulator = Add(accumulator, bi_toAdd, cachedNSq);
+            accumulatorActual = (accumulatorActual * biActual_toAdd) % cachedNSq;
+            accumulatorNeg = (accumulatorNeg * biNegative_toAdd) % cachedNSq;
         }
 
         public void Merge(PaillierAggregateSum anotherInstance)
@@ -46,76 +56,61 @@ namespace PrismaDB.MSSQL.AggregateUDFs
 
             if (isEmpty)
             {
-                accumulator = anotherInstance.accumulator;
+                accumulatorActual = anotherInstance.accumulatorActual;
+                accumulatorNeg = anotherInstance.accumulatorNeg;
                 cachedNSq = anotherInstance.cachedNSq;
                 isEmpty = false;
             }
             else
             {
-                accumulator = Add(accumulator, anotherInstance.accumulator, cachedNSq);
+                accumulatorActual = (accumulatorActual * anotherInstance.accumulatorActual) % cachedNSq;
+                accumulatorNeg = (accumulatorNeg * anotherInstance.accumulatorNeg) % cachedNSq;
             }
         }
 
         public SqlBytes Terminate()
         {
-            return new SqlBytes(accumulator);
+            var resActual = accumulatorActual.ToByteArray();
+            var resNegative = accumulatorNeg.ToByteArray();
+
+            var res = new byte[resActual.Length * 2];
+            Array.Copy(resActual, 0, res, 0, resActual.Length);
+            Array.Copy(resNegative, 0, res, res.Length / 2, resNegative.Length);
+
+            return new SqlBytes(res);
         }
 
         public void Read(BinaryReader r)
         {
             isEmpty = r.ReadBoolean();
-            var accLength = r.ReadInt32();
-            var accBytes = r.ReadBytes(accLength);
+            var accActualLength = r.ReadInt32();
+            var accActualBytes = r.ReadBytes(accActualLength);
+            var accNegLength = r.ReadInt32();
+            var accNegBytes = r.ReadBytes(accNegLength);
             var NsqLength = r.ReadInt32();
             var NsqBytes = r.ReadBytes(NsqLength);
 
-            accumulator = accBytes;
+            accumulatorActual = new BigInteger(accActualBytes);
+            accumulatorNeg = new BigInteger(accNegBytes);
             cachedNSq = new BigInteger(NsqBytes);
         }
 
         public void Write(BinaryWriter w)
         {
-            var accBytes = accumulator;
-            Int32 accLength = accBytes.Length;
+            var accActualBytes = accumulatorActual.ToByteArray();
+            Int32 accActualLength = accActualBytes.Length;
+            var accNegBytes = accumulatorNeg.ToByteArray();
+            Int32 accNegLength = accNegBytes.Length;
             var NsqBytes = cachedNSq.ToByteArray();
             Int32 NsqLength = NsqBytes.Length;
 
             w.Write(isEmpty);
-            w.Write(accLength);
-            w.Write(accBytes);
+            w.Write(accActualLength);
+            w.Write(accActualBytes);
+            w.Write(accNegLength);
+            w.Write(accNegBytes);
             w.Write(NsqLength);
             w.Write(NsqBytes);
-        }
-
-        private static byte[] Add(byte[] first, byte[] second, BigInteger NSquare)
-        {
-            var firstActual = new byte[first.Length / 2];
-            Array.Copy(first, firstActual, first.Length / 2);
-            var firstNegative = new byte[first.Length / 2];
-            Array.Copy(first, first.Length / 2, firstNegative, 0, first.Length / 2);
-            var secondActual = new byte[second.Length / 2];
-            Array.Copy(second, secondActual, second.Length / 2);
-            var secondNegative = new byte[second.Length / 2];
-            Array.Copy(second, second.Length / 2, secondNegative, 0, second.Length / 2);
-
-            var addActual = AddParts(firstActual, secondActual, NSquare);
-            var addNegative = AddParts(firstNegative, secondNegative, NSquare);
-
-            var add = new byte[first.Length];
-            Array.Copy(addActual, 0, add, 0, addActual.Length);
-            Array.Copy(addNegative, 0, add, add.Length / 2, addNegative.Length);
-
-            return add;
-        }
-
-        private static byte[] AddParts(byte[] first, byte[] second, BigInteger NSquare)
-        {
-            var A = new BigInteger(first);
-            var B = new BigInteger(second);
-
-            var resBi = (A * B) % NSquare;
-            var res = resBi.ToByteArray();
-            return res;
         }
     }
 }
